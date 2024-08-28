fc96a4d63ce9898f3e8fb6ecfbfbfb8fd51268f0
Joaquín <joacomk@gmail.com>
2024-08-28 15:53:31 -0300

    debug del programa principal de la calculadora

--- calculadora.py
+++ calculadora.py
@@ -6,15 +6,15 @@ from division import division
 def main():
     num1 = float(input("Ingresa el primer número: "))
     num2 = float(input("Ingresa el segundo número: "))
-    operator = input("Ingresa el operador (+. -. *. /): ")
+    operator = input("Ingresa el operador (+, -, *, /): ")
 
-    if opcion == "+":
+    if operator == "+":
         print(f"Resultado: {suma(num1, num2)}")
-    elif opcion == "-":
+    elif operator == "-":
         print(f"Resultado: {resta(num1, num2)}")
-    elif opcion == "*":
+    elif operator == "*":
         print(f"Resultado: {multiplicacion(num1, num2)}")
-    elif opcion == "/":
+    elif operator == "/":
         print(f"Resultado: {division(num1, num2)}")
     else:
         print("Opción no válida")
