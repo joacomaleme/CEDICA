403b4a6da3c03a42110cb43f9991326e8164d894
Joaquín <joacomk@gmail.com>
2024-09-05 14:06:22 -0300

    test del home para el hola mundo

---
+++ admin/tests/test_home.py
@@ -0,0 +1,11 @@
+from src.web import create_app
+
+app = create_app()
+app.testing = True
+
+client = app.test_client()
+
+def test_home():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.data == b"Hola mundo!"
