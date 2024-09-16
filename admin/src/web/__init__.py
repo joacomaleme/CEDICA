17b76f0b141d320af2c3fc8abd19b5e09186a0ef
Ivy Daguerre Elgarte <ide088re@gmail.com>
2024-09-16 16:34:26 -0300

    Todo MVC y config

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template
 from src.web.handlers import error
+from src.web.config import config
+from src.web.controllers.datos_reales import bp as datos_reales_bp
 
 def create_app(env="development", static_folder="../../static"):
     app = Flask(__name__, static_folder=static_folder)
+    app.config.from_object(config[env])
 
     @app.route("/")
     def home():
         return render_template('home.html')
     
+    app.register_blueprint(datos_reales_bp)
+
     app.register_error_handler(404, error.error_not_found)
 
     return app
---
+++ admin/src/web/controllers/datos_reales.py
@@ -0,0 +1,11 @@
+from flask import render_template
+from flask import Blueprint
+from src.model import board
+
+bp = Blueprint("datos_reales", __name__, url_prefix="/dataSecreta")
+
+@bp.get("/")
+def index():
+    datos = board.list_datos_reales()
+
+    return render_template("datos_reales/index.html", datos_reales = datos)
