6851b24b9adabbfcc78517762a011f0af51e1c60
Joaquin Malchansky Saleme <joacomk8@gmail.com>
2024-10-10 21:08:40 -0300

    configuracion basica de MinIO

--- admin/src/model/config.py
+++ admin/src/model/config.py
@@ -7,9 +7,17 @@ class Config(object):
     
 
 class ProductionConfig(Config):
+    MINIO_SERVER = environ.get("MINIO_SERVER")
+    MINIO_ACCESS_KEY = environ.get("MINIO_ACCESS_KEY")
+    MINIO_SECRET_KEY = environ.get("MINIO_SECRET_KEY")
+    MINIO_SECURE = True
     SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
 
 class DevelopmentConfig(Config):
+    MINIO_SERVER = "localhost:9000"
+    MINIO_ACCESS_KEY = "xdg4qu971EtGU1dHCwRA"
+    MINIO_SECRET_KEY = "vAWRO47HxY0AMztHIvrWj7Z3PgdD9ZuoXEEkEyf5"
+    MINIO_SECURE = False
     DB_USER = "postgres"
     DB_PASSWORD = "postgres"
     DB_HOST = "localhost"
