4898b4bc9002f20a8c2071e0fdbcc4fbea8c7f3f
SANTIAGO CHAVES <santiagochaves142@gmail.com>
2024-10-12 22:55:12 -0300

    Checkeos controller agregar empleado

--- admin/src/web/controllers/employee_controller.py
+++ admin/src/web/controllers/employee_controller.py
@@ -1,8 +1,9 @@
 from flask import redirect, render_template, request, url_for
-from flask import Blueprint
+from flask import Blueprint, flash
 from src.model.employees.operations import employee_operations
 from src.model.employees.operations import job_position_operations
 from src.model.employees.operations import profession_operations
+import re
 
 bp = Blueprint("employee", __name__, url_prefix="/empleados")
 
@@ -42,10 +43,21 @@ def create():
         "emergency_contact_phone": params.get("emergency-contact-phone"),
         "obra_social": params.get("obra-social"),
         "affiliate_number": params.get("affiliate-number"),
-        "is_volunteer": params.get("volunteer") == 'on',  # Convert to bool
+        "is_volunteer": params.get("volunteer") != None,  # Convert to bool
         "start_date": params.get("start-date"),
         "end_date": params.get("end-date"),
     }
+
+    check_dni = employee_operations.get_employee_by_dni(employee_data["dni"])
+    check_mail = employee_operations.get_employee_by_email(employee_data["email"])
+    check_affiliate_number = employee_operations.get_employee_by_affiliate_number(employee_data["affiliate_number"])
+
+    if check_dni or check_mail or check_affiliate_number:
+        flash("Lo lamentamos, ha habido un error inesperado", "error")
+        return redirect((url_for("employee.new")))
+    if (not is_valid_email(employee_data["email"])):
+        flash("La dirección de mail ingresada no es válida", "error")
+        return redirect((url_for("employee.new")))
 
     employee_operations.create_employee(**employee_data)
 
@@ -63,3 +75,7 @@ def update(id):
 def delete(id):
     employee_operations.delete_employee(id)
     return redirect(url_for("employee.index"))
+
+def is_valid_email(email:str):
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    return re.match(pattern, email) is not None
