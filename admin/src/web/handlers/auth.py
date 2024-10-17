e5d38fce8f3659cd836bbbade6d92828ac0ba224
Ivy Daguerre Elgarte <ide088re@gmail.com>
2024-10-16 23:00:03 -0300

    Bugfix permisos

--- admin/src/web/handlers/auth.py
+++ admin/src/web/handlers/auth.py
@@ -4,7 +4,7 @@ def is_authenticated(session):
   return session.get("user") is not None
 
 def is_permitted(session, permission:str) -> bool:
-  return has_permission(session.get("user"), permission)
+  return is_authenticated(session) and has_permission(session.get("user"), permission)
 
 def is_self(session, email:str) -> bool:
   return session.get("user") == email
